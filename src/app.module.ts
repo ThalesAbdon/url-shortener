1f0b760e2bb3b20a3fd45b695f5290dbe0d18867
Thales Abdon <thales.sousufpi@gmail.com>
2026-03-17 20:04:33 -0300

    feat: add rate limiting with @nestjs/throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { Url } from './urls/entities/url.entity';
 import { UrlsModule } from './urls/urls.module';
 
@@ -19,7 +21,25 @@ import { UrlsModule } from './urls/urls.module';
         ssl: false,
       }),
     }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,   
+        limit: 3,    
+      },
+      {
+        name: 'long',
+        ttl: 60000,  
+        limit: 100,  
+      },
+    ]),
     UrlsModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
